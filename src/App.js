f9c1649408acb8bcf028f04cfc41b70e1e9fc8fe
Trigo YODILA <trigoyodila1996@gmail.com>
2022-08-25 16:01:01 +0100

    fixbug redirect-uri

--- src/App.js
+++ src/App.js
@@ -7,11 +7,11 @@ import "./style.css";
 const spotify = new SpotifyWebApi();
 
 //ac861758db1f4152aeee2f814db1a478 (local client) http://localhost:3000/dashboard
-//232d842a255c4fd3b756903ee946495d (inline client) https://bmusic.vercel.app/
+//232d842a255c4fd3b756903ee946495d (inline client) https://bmusic.vercel.app/dashboard
 function App() {
   const userAuth = {
     CLIENT_ID: "232d842a255c4fd3b756903ee946495d",
-    REDIRECT_URI: "https://bmusic.vercel.app",
+    REDIRECT_URI: "https://bmusic.vercel.app/dashboard",
     AUTH_ENDPOINT: "https://accounts.spotify.com/authorize",
     RESPONSE_TYPE: "token",
   };
