fda748bed4a2ffdc07420971b0a32ab38f8e9558
Trigo YODILA <trigoyodila1996@gmail.com>
2022-08-26 16:51:02 +0100

    add loading data before get data api

--- src/components/Recents.js
+++ src/components/Recents.js
@@ -4,10 +4,11 @@ import "../styles/recents.css";
 import { globalData } from "./userContext";
 import SpotifyWebApi from "spotify-web-api-js";
 import { DashContext } from "./DashboardUseContext";
-
+import CircularProgress from "@mui/material/CircularProgress";
+import Box from "@mui/material/Box";
 
 const style = {
-  marginBottom:"1rem"
+  marginBottom: "1rem",
 };
 
 const spotify = new SpotifyWebApi();
@@ -16,55 +17,51 @@ export default function Recents() {
   const { token } = useContext(globalData);
 
   const [recents, setRecents] = useState([]);
-  const [clicked, setClicked] = useState(true);
+  const [clicked, setClicked] = useState(false);
+  const [isloading, setIsLoading] = useState(true);
   const { trackuri, setTrackuri } = useContext(DashContext);
 
   spotify.setAccessToken(token);
 
   useEffect(() => {
-   
-      clicked
-        ? spotify
-            .getMyRecentlyPlayedTracks({ limit: 4 })
-            .then((data) => {
-              console.log(data.items);
-              setRecents(data.items);
-              console.log("Artist albums", data.items[0].track.artists[0].name);
-              // console.log("Data", data);
-              console.log("Artist albums", data.items[0].track.name);
-              console.log(
-                "url image ",
-                data.items[0].track.album.images[0].url
-              );
-            })
-            .catch((err) => {
-              console.log(err);
-            })
-        : spotify
-            .getMyRecentlyPlayedTracks()
-            .then((data) => {
-              console.log(data.items);
-              setRecents(data.items);
-              console.log("Artist albums", data.items[0].track.artists[0].name);
-              // console.log("Data", data);
-              console.log("Artist albums", data.items[0].track.name);
-              console.log(
-                "url image ",
-                data.items[0].track.album.images[0].url
-              );
-            })
-            .catch((err) => {
-              console.log(err);
-            });
-  }, [clicked,token]);
+    !clicked
+      ? spotify
+          .getMyRecentlyPlayedTracks({ limit: 4 })
+          .then((data) => {
+            console.log(data.items);
+            setRecents(data.items);
+            setIsLoading(false);
+            console.log("Artist albums", data.items[0].track.artists[0].name);
+            // console.log("Data", data);
+            console.log("Artist albums", data.items[0].track.name);
+            console.log("url image ", data.items[0].track.album.images[0].url);
+          })
+          .catch((err) => {
+            console.log(err);
+          })
+      : spotify
+          .getMyRecentlyPlayedTracks()
+          .then((data) => {
+            console.log(data.items);
+            setRecents(data.items);
+            setIsLoading(false);
+            console.log("Artist albums", data.items[0].track.artists[0].name);
+            // console.log("Data", data);
+            console.log("Artist albums", data.items[0].track.name);
+            console.log("url image ", data.items[0].track.album.images[0].url);
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+  }, [clicked, token]);
 
   console.log("externe", recents);
 
   function handleClicked() {
     setClicked(!clicked);
+    setIsLoading(true);
   }
 
-  
   return (
     <div style={style} className="container-recents">
       <div className="title-card">
@@ -74,22 +71,33 @@ export default function Recents() {
         </span>
       </div>
       <div className="recents-card">
-        {recents.map((item) => {
-          return (
-            <div className="recent-item">
-              <img src={item.track.album.images[0].url} />
-              <div className="item-info">
-                <div className="track-name-title">
-                  <div className="title-track">{item.track.name}</div>
-                  <div className="name-track">{item.track.artists[0].name}</div>
-                </div>
-                <div className="play" onClick={() => setTrackuri(item.track.uri)}>
-                  <div className="arrow-right"></div>
+        {isloading ? (
+          <Box sx={{ display: "flex" }}>
+            <CircularProgress color="error" />
+          </Box>
+        ) : (
+          recents.map((item) => {
+            return (
+              <div className="recent-item">
+                <img src={item.track.album.images[0].url} />
+                <div className="item-info">
+                  <div className="track-name-title">
+                    <div className="title-track">{item.track.name}</div>
+                    <div className="name-track">
+                      {item.track.artists[0].name}
+                    </div>
+                  </div>
+                  <div
+                    className="play"
+                    onClick={() => setTrackuri(item.track.uri)}
+                  >
+                    <div className="arrow-right"></div>
+                  </div>
                 </div>
               </div>
-            </div>
-          );
-        })}
+            );
+          })
+        )}
       </div>
     </div>
   );
